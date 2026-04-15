16794dd6fe4c54d3513867b659c1a2ea5edaa5c1
aonmd <aonmd76@gmail.com>
2026-04-16 03:54:14 +1000

    Added prerequisite feature

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 package main.java;
 
+// Main program
 public class Main {
     public static void main(String[] args) {
 
-        // Create students
         Student s1 = new Student("S1", "Aon", "aon@email.com", "Java");
         Student s2 = new Student("S2", "John", "john@email.com", "None");
-        // Create courses
+
         Course c1 = new Course("C1", "Advanced Java", 2, "Java");
         Course c2 = new Course("C2", "Math", 3, "None");
-        // Create enrolments
+
         Enrolment e1 = new Enrolment(s1, c1, "2026-04-14");
         Enrolment e2 = new Enrolment(s2, c2, "2026-04-14");
 
-        // Display enrolments
         e1.display();
         System.out.println("----------------");
         e2.display();
